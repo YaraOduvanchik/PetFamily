21c3cfb0997bb15e51c60315c07c5bd7d4dae568
YaraOduvanchik <Cherashev4@gmail.com>
2024-08-16 11:54:47 +0300

    Create Pet

---
+++ PetFamily.Backend/src/PetFamily.Domain/Models/Pet.cs
@@ -0,0 +1,75 @@
+﻿namespace PetFamily.Domain.Models;
+
+public sealed class Pet
+{
+    public Pet(
+        Guid id,
+        string name,
+        string type,
+        string description,
+        string breed,
+        string color,
+        string healthInfo,
+        string address,
+        double weight,
+        double height,
+        string phoneNumber,
+        bool isCastrated,
+        DateTimeOffset birthDate,
+        bool isVaccine,
+        string helpStatus,
+        DateTimeOffset createdDate)
+    {
+        Id = id;
+        Name = name;
+        Type = type;
+        Description = description;
+        Breed = breed;
+        Color = color;
+        HealthInfo = healthInfo;
+        Address = address;
+        Weight = weight;
+        Height = height;
+        PhoneNumber = phoneNumber;
+        IsCastrated = isCastrated;
+        BirthDate = birthDate;
+        IsVaccine = isVaccine;
+        HelpStatus = helpStatus;
+        CreatedDate = createdDate;
+    }
+
+    public Guid Id { get; private set; }
+
+    public string Name { get; private set; }
+
+    public string Type { get; private set; }
+
+    public string Description { get; private set; }
+
+    public string Breed { get; private set; }
+
+    public string Color { get; private set; }
+
+    public string HealthInfo { get; private set; }
+
+    public string Address { get; private set; }
+    
+    public double Weight { get; private set; }
+    
+    public double Height { get; private set; }
+
+    public string PhoneNumber { get; private set; }
+    
+    public bool IsCastrated { get; private set; }
+    
+    public DateTimeOffset BirthDate { get; private set; }
+    
+    public bool IsVaccine { get; private set; }
+
+    public string HelpStatus { get; private set; }
+
+    public IReadOnlyList<Requisite> Requisites => _requisites;
+    private List<Requisite> _requisites = [];
+    
+    public DateTimeOffset CreatedDate { get; private set; }
+}
