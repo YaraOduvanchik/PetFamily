d0b49fe37236df5c8ff79fc83ea7d2c29cff3d2a
YaraOduvanchik <Cherashev4@gmail.com>
2024-08-16 12:28:34 +0300

    Create Volunteer

---
+++ PetFamily.Backend/src/PetFamily.Domain/Models/SocialNetwork.cs
@@ -0,0 +1,26 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace PetFamily.Domain.Models;
+
+public record SocialNetwork
+{
+    private SocialNetwork(string title, string link)
+    {
+        Title = title;
+        Link = link;
+    }
+
+    public string Title { get; }
+    public string Link { get; }
+
+    public static Result<SocialNetwork> Create(string title, string link)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            return Result.Failure<SocialNetwork>("Title is empty");
+
+        if (string.IsNullOrWhiteSpace(link))
+            return Result.Failure<SocialNetwork>("Link is empty");
+
+        return new SocialNetwork(title, link);
+    }
+}
