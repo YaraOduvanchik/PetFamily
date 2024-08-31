a3e85d6f78e10fdf423d3da1fee867605bdfce72
YaraOduvanchik <Cherashev4@gmail.com>
2024-08-31 22:24:15 +0500

    Added ExceptionMiddleware and Seq

--- PetFamily.Backend/src/PetFamily.API/Program.cs
+++ PetFamily.Backend/src/PetFamily.API/Program.cs
@@ -1,21 +1,42 @@
 using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.HttpLogging;
+using PetFamily.API;
+using PetFamily.API.Extensions;
+using PetFamily.API.Middlewares;
 using PetFamily.API.Validation;
 using PetFamily.Application;
 using PetFamily.Infrastructure;
+using Serilog;
+using Serilog.Events;
 using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.Debug()
+    .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq") 
+                 ?? throw new ArgumentNullException("Seq"))
+    .Enrich.WithThreadId()
+    .Enrich.WithEnvironmentName()
+    .Enrich.WithMachineName()
+    .Enrich.WithEnvironmentUserName()
+    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+    .CreateLogger();
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSerilog();
+
 builder.Services
     .AddInfrastructure()
     .AddApplication();
-
-ValidatorOptions.Global.LanguageManager.Enabled = false;
 
 builder.Services.AddFluentValidationAutoValidation(configuration =>
 {
@@ -24,11 +45,17 @@ builder.Services.AddFluentValidationAutoValidation(configuration =>
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    await app.AddMigrations();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
